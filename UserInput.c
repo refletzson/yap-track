d42a1d88ab377552e7127828cca55a856b3d9aa6
unknown <esojhrenzo12@gmail.com>
2025-09-01 13:19:07 -0300

    first commit

---
+++ UserInput.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int age = 0;
+    float gpa = 0.0f;
+    char grade = '\0';
+    char name [30] = "";
+
+    printf("%d\n",age);
+    printf("%f\n",gpa);
+    printf("%c\n",grade);
+    printf("%s\n",name);
+   
+    return 0;
+}
