b7083d3a03cb2394bbe415d717f847a20205f967
refletzson <esojhrenzo12@gmail.com>
2026-04-01 16:12:05 -0300

    add vercel config for SPA routing

---
+++ Desktop/Learning Code/UserInput/UserInput.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+printf("test123");
+
+    int age = 0;
+    float gpa = 0.0f;
+    char grade = '\0';
+    char name [30] = "";
+
+    printf("%d\n",age);
+    printf("%f\n",gpa);
+    printf("%c\n",grade);
+    printf("%s\n",name);
+   
+    return 0;
+}
