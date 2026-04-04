37120d908a4f4e9921329b103e605099814df4bf
refletzson <esojhrenzo12@gmail.com>
2026-04-03 21:23:29 -0300

    use DB_PATH env var for persistent database location

--- OneDrive/Documents/YapTrack/backend/db.js
+++ OneDrive/Documents/YapTrack/backend/db.js
@@ -1,7 +1,8 @@
 const { DatabaseSync } = require('node:sqlite')
 const path = require('path')
 
-const db = new DatabaseSync(path.join(__dirname, 'yaptrack.db'))
+const dbPath = process.env.DB_PATH || path.join(__dirname, 'yaptrack.db')
+const db = new DatabaseSync(dbPath)
 
 db.exec('PRAGMA journal_mode = WAL')
 db.exec('PRAGMA foreign_keys = ON')
